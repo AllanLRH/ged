24bc2a170c1d8a2c160d1d4ff88d93171458b65b
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-04-20 14:01:27 +0200

    First version of leadGed.m

--- loadGed.m
+++ loadGed.m
@@ -1,14 +1,50 @@
-clear all; close all; clc;
+function imageData = loadGed(imageName, varargin)
+%LOADGED Loads image
+%   imageName is the data file
+%   Optional argument 1 is selected range of images (vector, counting from 1)
+%   Optional argument 2 is data formatting string
 
-h = fopen('/Users/allan/migMount/eScience/XRay/apu05.esci.nbi.dk.0_localhost/tandlaege/data/volfloat/5.05_ID1662_769_pag0001.vol', 'r');
-% dims = [2048 2048 256];
-dims = [2048 2048 1];
-img = fread(h, prod(dims), 'float32');
-img = reshape(img, dims);
-%% Show image
+% Extract arguments
+idx = 0;
+if ~isempty(varargin) && isnumeric(varargin{1})
+    idx = varargin{1};
+end
 
-imagesc(img(:, :, 1))
-colormap gray
+if ~isempty(varargin)
+    if ischar(varargin{1})
+        formatString = varargin{1};
+    elseif length(varargin) == 2 && ischar(varargin{2})
+        formatString = varargin{2};
+    else
+        formatString = 'float=>double';
+    end
+end
 
+% Open image and preallocate
+fid = fopen(imageName, 'r');
+if fid < 1
+    if exist(Name, 'file') ~= 2
+        error('File did not exist')
+    end
+    error(['File did not load correctly! ' imageName])
+end
+dims = [2048 2048];
+imageData = zeros([2048 2048 length(idx)]);
 
+% Load image
+if idx(1) > 0
+    cnt = 1;
+    for i = idx
+        fseek(fid, (prod(dims))^2*i-1, 'bof');
+        imageData(:, :, cnt) = reshape(fread(fid, prod(dims), formatString), dims);
+        cnt = cnt + 1;
+    end
+else
+    imageData = reshape(fread(fid, prod(dims), formatString), dims);
+end
+
+% close image file
+fclose(fid);
+
+end
 
