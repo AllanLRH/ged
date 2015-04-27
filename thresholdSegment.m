a791c93b0bbcbcfe2836db5f9a6e4e05c05e768e
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-04-27 17:25:25 +0200

    Make filter plot matrix

--- thresholdSegment.m
+++ thresholdSegment.m
@@ -13,5 +13,25 @@ shadeArea(imp2, [1 0 0]);
 
 %% Find bone/soft tissue
 
-
-
+imf = fspecial('gaussian', 4, 2);
+imgB2 = imfilter(imfilter(img, imf), imf);
+imgB3 = medfilt2(imgB2, [4,4]);
+bone1 = imgB3 < 0.10;
+figure
+cnt = 1;
+for ii = 1:2:10
+    for jj = 1:2:10
+        subplot(5, 5, cnt)
+        se = strel('disk', ii);
+        bone2 = imopen(bone1, se);
+        se = strel('disk', jj);
+        bone3 = imclose(bone2, se);
+        imagesc(img)
+        shadeArea(bone3, [0 0 1])
+        cnt = cnt + 1;
+        t = title(sprintf('open %d, close %d', ii, jj));
+        t.FontSize = 10;
+        t.FontWeight = 'normal';
+        colormap gray
+    end
+end
