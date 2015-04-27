2a8d85c7ba813a3e9fc50140cf234780bfabc8dd
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-04-27 17:24:35 +0200

    Updated naming of masks and added color argument to shading

--- thresholdSegment.m
+++ thresholdSegment.m
@@ -3,13 +3,13 @@ clear all; close all; clc
 img = loadGed('/Users/allan/migMount/eScience/XRay/apu05.esci.nbi.dk.0_localhost/tandlaege/data/volfloat/5.05_ID1662_769_pag0001.vol', 1);
 %% Find implant
 close all
-mask1 = img < 0.62;
+imp1 = img < 0.62;
 scMin = min(img(:));
 scMax = max(img(:));
 se = strel('disk', 18);
-mask2 = imclose(mask1, se);
+imp2 = ~imclose(imp1, se);
 h = imshow(img, [scMin, scMax]);
-shadeArea(mask2);
+shadeArea(imp2, [1 0 0]);
 
 %% Find bone/soft tissue
 
