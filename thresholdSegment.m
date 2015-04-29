c41e30190d553bfb1c85097f65b21776ede421dd
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-04-29 14:52:06 +0200

    Using local data instead if Mig data

--- thresholdSegment.m
+++ thresholdSegment.m
@@ -1,6 +1,7 @@
 clear all; close all; clc
 
-img = loadGed('/Users/allan/migMount/eScience/XRay/apu05.esci.nbi.dk.0_localhost/tandlaege/data/volfloat/5.05_ID1662_769_pag0001.vol', 1);
+% img = loadGed('/Users/allan/migMount/eScience/XRay/apu05.esci.nbi.dk.0_localhost/tandlaege/data/volfloat/5.05_ID1662_769_pag0001.vol', 1);
+img = loadGed('5.05_ID1662_769_pag0001.vol', 1);
 %% Find implant
 close all
 imp1 = img < 0.62;
