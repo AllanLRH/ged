62ca2f8dbc5b92793a2c4c5f32d8e5fdba633427
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-05-11 17:49:11 +0200

    Updated shadeLinker

--- shadeLinker.m
+++ shadeLinker.m
@@ -2,16 +2,32 @@ function h = shadeLinker( img, mask, varargin )
 %shadeLinker subplot with shaded and original image with linked axes
 
 h = figure;
-ha(1) = subplot(121);
-imsc(img)
-shadeArea(mask, [1 0 0]);
-ha(2) = subplot(122);
-if nargin > 2 && strcmp(varargin{1}, 'mask')
-    imsc(mask)
-else
+maskColor = [0 0 1];
+if nargin > 2  % Non-default behaviour
+    if strcmpi(varargin{1}, 'imgAndMask')  % Plot mask left, image right
+        ha(1) = subplot(121);
+        imsc(mask)
+        ha(2) = subplot(122);
+        imsc(img)
+    elseif strcmpi(varargin{1}, 'shadeAndMask')  % Plot shaded image left, image right
+        ha(1) = subplot(121);
+        imsc(img)
+        shadeArea(mask, maskColor);
+        ha(2) = subplot(122);
+        imsc(mask)
+    end
+else  % Default behaviour: Plot shaded image left, image right
+    ha(1) = subplot(121);
+    imsc(img)
+    shadeArea(mask, maskColor);
+    ha(2) = subplot(122);
     imsc(img)
 end
+
 linkaxes(ha, 'xy');
 maximize
 
 end
+
+
+
