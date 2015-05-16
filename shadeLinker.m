95c388c4de938b2839ebc85f6f31099e71681e93
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-05-16 22:08:54 +0200

    Updated shadeLinker.m to be more flexible in regards to user string-settings and to only return figure handle if explicitly asked for

--- shadeLinker.m
+++ shadeLinker.m
@@ -1,15 +1,15 @@
-function h = shadeLinker( img, mask, varargin )
+function varargout = shadeLinker( img, mask, varargin )
 %shadeLinker subplot with shaded and original image with linked axes
 
 h = figure;
 maskColor = [0 0 1];
 if nargin > 2  % Non-default behaviour
-    if strcmpi(varargin{1}, 'imgAndMask')  % Plot mask left, image right
+    if strcmpi(varargin{1}, 'imgAndMask') || strcmpi(varargin{1}, 'maskAndImg')  % Plot mask left, image right
         ha(1) = subplot(121);
         imsc(mask)
         ha(2) = subplot(122);
         imsc(img)
-    elseif strcmpi(varargin{1}, 'shadeAndMask')  % Plot shaded image left, image right
+    elseif strcmpi(varargin{1}, 'shadeAndMask') || strcmpi(varargin{1}, 'maskAndShade')  % Plot shaded image left, image right
         ha(1) = subplot(121);
         imsc(img)
         shadeArea(mask, maskColor);
@@ -22,6 +22,10 @@ else  % Default behaviour: Plot shaded image left, image right
     shadeArea(mask, maskColor);
     ha(2) = subplot(122);
     imsc(img)
+end
+
+if nargout == 1
+    varargout{1} = h;
 end
 
 linkaxes(ha, 'xy');
