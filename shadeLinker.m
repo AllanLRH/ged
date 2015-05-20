2217561075a0e05a2303c86f2355d3d601e08f5d
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-05-20 14:16:32 +0200

    More intuitive placement of plots (right-left) in shadeLinker.m

--- shadeLinker.m
+++ shadeLinker.m
@@ -6,15 +6,15 @@ maskColor = [0 0 1];
 if nargin > 2  % Non-default behaviour
     if strcmpi(varargin{1}, 'imgAndMask') || strcmpi(varargin{1}, 'maskAndImg')  % Plot mask left, image right
         ha(1) = subplot(121);
-        imsc(mask)
-        ha(2) = subplot(122);
         imsc(img)
+        ha(2) = subplot(122);
+        imsc(mask)
     elseif strcmpi(varargin{1}, 'shadeAndMask') || strcmpi(varargin{1}, 'maskAndShade')  % Plot shaded image left, image right
         ha(1) = subplot(121);
         imsc(img)
-        shadeArea(mask, maskColor);
         ha(2) = subplot(122);
         imsc(mask)
+        shadeArea(mask, maskColor);
     end
 else  % Default behaviour: Plot shaded image left, image right
     ha(1) = subplot(121);
