4f7724ce740d0d9040c2d1bbbad600dfa51a6806
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-05-27 16:40:06 +0200

    Using previous image mask on subsequent images. Seems to work ok, but there's a switch in what's identified

--- boneFraction.m
+++ boneFraction.m
@@ -1,27 +1,99 @@
 clear; close all; clc
 
-img = normImage(loadGed('5.05_ID1662_769_0001.vol', 50));
 nBands = 100;
-[r, c] = size(img);
-bandBorders = linspace(0, r/2, nBands+1);
-boneVolumeFraction = zeros(1, nBands);
-
-[implant, bone, cavity] = thresholdSegment(img);
+boxsize = 5;
 load('circ.mat')
+circArea     = sum(circ(:));
 
-dstMap = sgnDstFromImg(implant);
-for ii = 1:nBands
-    dstMask = (bandBorders(ii) > dstMap) & (dstMap > 0);
-    boneVolumeFraction(ii) = sum(bone(dstMask))/(pi*bandBorders(ii)^2);% * 1/numel(dstMask);
-    % imsc(dstMask)
-    % pause(0.01)
+im1 = normImage(loadGed('5.05_ID1662_769_0001.vol', 1));
+s = size(im1, 1);  %  Quadratic image
+
+implantMask  = thresholdSegment(im1);
+interestMask = (circ & ~implantMask);
+bias         = biasCorrect(im1, interestMask);
+im1          = im1 - bias;
+
+boneMask     = logical(mean(imread('darkMask.tiff'), 3));
+cavityMask   = logical(mean(imread('lightMask.tiff'), 3));
+
+boneStd      = std(im1(boneMask));
+boneMean     = mean(im1(boneMask));
+cavityStd    = std(im1(cavityMask));
+cavityMean   = mean(im1(cavityMask));
+
+meanImg      = getMeanImage(im1, boxsize);
+stdImg       = getStdImage(im1, boxsize, meanImg);
+
+bone1        = (meanImg-boneMean).^2+(stdImg-boneStd).^2;
+cavity1      = (meanImg-cavityMean).^2+(stdImg-cavityStd).^2;
+mask1        = (bone1 > cavity1).*interestMask;
+
+seCleaner         = strel('disk', 4);
+mask2             = imclose(mask1, seCleaner);
+
+seNextImg         = strel('disk', 7);
+boneMaskNextImg   = logical(imopen(mask1, seNextImg));
+cavityMaskNextImg = logical(imopen((bone1 < cavity1).*interestMask, seNextImg));
+
+shadeLinker(im1, mask2, 'shadeAndMask')
+
+%% Process next image
+for ii = 2:10
+
+    im2 = normImage(loadGed('5.05_ID1662_769_0001.vol', ii));
+
+    boneStd    = std(im2(boneMaskNextImg));
+    boneMean   = mean(im2(boneMaskNextImg));
+    cavityStd  = std(im2(cavityMaskNextImg));
+    cavityMean = mean(im2(cavityMaskNextImg));
+
+    meanImg    = getMeanImage(im2, boxsize);
+    stdImg     = getStdImage(im2, boxsize, meanImg);
+
+    bone2      = (meanImg-boneMean).^2+(stdImg-boneStd).^2;
+    cavity2    = (meanImg-cavityMean).^2+(stdImg-cavityStd).^2;
+    mask3      = (bone2 > cavity2).*interestMask;
+
+    mask4             = imclose(mask3, seCleaner);
+    boneMaskNextImg   = logical(imopen(mask4, seNextImg));
+    cavityMaskNextImg = logical(imopen((bone2 < cavity2).*interestMask, seNextImg));
+
+    shadeLinker(im2, mask4, 'shadeAndMask')
+
 end
-plot(boneVolumeFraction)
 
-% Check that there's no summing going on outside the circular image
 
-% Don't sum in the implant
-
-% Limut summing to first half of image
-
-% Reproduce Torstens graph
+%%  De statistics
+% [xi, yi] = find(implantMask);
+% dstX = min(min(xi), s-max(xi));
+% dstY = min(min(yi), s-max(yi));
+% maxRadius = min(dstX, dstY);
+% % centroid = regionprops(implantMask, 'centroid');
+% % centroid = round(centroid.Centroid);
+% % maxRadius = min(abs([centroid centroid - s]));
+% % bandBorders = round(maxRadius/nBands : maxRadius/nBands : maxRadius*(1 - 1/nBands));
+% bandBorders = floor(maxRadius/nBands*(1:nBands));
+% boneVolumeFraction = zeros(1, nBands)*NaN;
+%
+%
+% dstMap = sgnDstFromImg(implantMask);
+% for ii = 1:nBands
+%     dstMask = (bandBorders(ii) > dstMap) & (dstMap > 0);
+%     if sum(sum(dstMask|circ)) > circArea
+%         warning('Circle outside image bounds for the %d''th iteration. Halting execution.', ii)
+%         break
+%     end
+%     boneVolumeFraction(ii) = sum(boneMask(dstMask))/(pi*bandBorders(ii)^2);% * 1/numel(dstMask);
+%     % imsc(dstMask)
+%     % pause(0.01)
+% end
+%
+% plot(boneVolumeFraction)
+%
+% % Check that there's no summing going on outside the circular image
+%
+% % Don't sum in the implant
+%
+% % Limut summing to first half of image
+%
+% % Reproduce Torstens graph
