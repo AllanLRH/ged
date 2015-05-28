a5f5fc7bb9718b20658a2b57b6d6da83496e5e15
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-05-28 21:53:26 +0200

    Added blurring, slow but seems to work.

--- segmentImplant.m
+++ segmentImplant.m
@@ -1,15 +1,19 @@
-function [imp4] = segmentImplant(meanImg, stdImg)
+function [imp5] = segmentImplant(img)
 %% segmentImplant: Return a mask covering the implant
-% meanImg is getMeanImgge(nm1, 5).^2
-% stdImg is  getStdImage(img, n).^2
     
-    meanStdImg = meanImg.^2 + stdImg.^2;
-    threshold = 0.33;
-    imp1 = meanStdImg > threshold;
-    % Remove small misclassified areas
-    se = strel('disk', 25);
-    imp2 = imopen(imp1, se);
-    imp3 = imclose(imp2, se);
-    % Fill the middle of the implant in case it's not
-    imp4 = logical(bwfill(imp3, 'holes'));
+    im2 = conv2(img, ones(10), 'same');
+    im3 = medfilt2(im2, 'symmetric');
+    circCoords = makeCircle(2048, 320, [1024, 1024]);
+    implantSlice = im3(circCoords);
+    % Assume that the implant is the most common value around
+    % the center of the image
+    implantValue = mode(median(implantSlice));
+    
+    imp1 = (im3 > 0.9*implantValue) & (im3 < 1.1*implantValue);
+    se = strel('disk', 10);
+    imp2 = imclose(imp1, se);
+    imp3 = imopen(imp2, se);
+    se = strel('disk', 45);
+    imp4 = imopen(imp3, se);
+    imp5 = logical(bwfill(imp4, 'holes'));
 end
