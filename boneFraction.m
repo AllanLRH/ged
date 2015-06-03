a1dd67a73c250da257dc4e686bfb5bf4a7af551d
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-06-03 17:57:38 +0200

    Moved statistics section into loop, error in summantion (overwritten in each iteration)

--- boneFraction.m
+++ boneFraction.m
@@ -1,6 +1,7 @@
 clear; %close all; clc
 
 nBands = 100;
+stackSize = 255;
 boxsize = 5;
 load('circ.mat')
 circArea     = sum(circ(:));
@@ -8,17 +9,21 @@ circArea     = sum(circ(:));
 im1 = normImage(loadGed('5.05_ID1662_769_0001.vol', 1));
 s = size(im1, 1);  %  Quadratic image
 
+savedImplantMasks = false(s, s, stackSize);
+savedBoneMasks = false(s, s, stackSize);
+
 % implantMask  = segmentImplant(im1);
 implantMask  = thresholdSegment(im1);
+savedImplantMasks(:, :, 1) = implantMask;
 interestMask = (circ & ~implantMask);
 bias         = biasCorrect(im1, interestMask);
 im1          = im1 - bias;
 
-boneMask     = logical(mean(imread('darkMask.tiff'), 3));
+mask4     = logical(mean(imread('darkMask.tiff'), 3));
 cavityMask   = logical(mean(imread('lightMask.tiff'), 3));
 
-boneStd      = std(im1(boneMask));
-boneMean     = mean(im1(boneMask));
+boneStd      = std(im1(mask4));
+boneMean     = mean(im1(mask4));
 cavityStd    = std(im1(cavityMask));
 cavityMean   = mean(im1(cavityMask));
 
@@ -31,6 +36,7 @@ mask1        = (bone1 > cavity1);
 
 seCleaner         = strel('disk', 4);
 mask2             = imclose(mask1, seCleaner) & interestMask;
+savedBoneMasks(:, :, 1) = mask2;
 
 seNextImg         = strel('disk', 5);
 boneMaskNextImg   = imerode(~mask1, seNextImg) & interestMask;  % why isn't the ~ on the cavityMaskNextImg?
@@ -39,13 +45,29 @@ cavityMaskNextImg = imerode(mask1, seNextImg) & interestMask;
 % shadeLinker(im1, mask2, 'shadeAndMask')
 
 load('desiredHistogram');
-cdfAbsDiffSum = zeros([1 255]);
+cdfAbsDiffSum = zeros([1 stackSize]);
 cdfAbsDiffSum(1) = sum(abs(desiredHistogram - img2cdf(im1)));
 
 
-%% Process next image
-for ii = 2:255
+[xi, yi] = find(implantMask);
+dstMap = sgnDstFromImg(implantMask);
+dstBorders = [dstMap(1:s, 1) dstMap(1:s, s) dstMap(1, 1:s)' dstMap(s, 1:s)'];
+maxRadius = floor(min(dstBorders(:)));
+bandBorders = linspace(0, maxRadius, nBands);
+boneVolume = zeros(nBands, 1);
+area = zeros(nBands, 1);
 
+for ii = 1:nBands
+    dstMask = (dstMap < bandBorders(ii)) & (dstMap > 0);
+    boneVolume(ii) = sum(mask4(dstMask));
+    area(ii) = sum(dstMask(:));
+    % imsc(dstMask)
+    % pause(0.01)
+end
+
+%% Process next image
+for ii = 2:stackSize
+    disp(ii/stackSize*100)
     im2 = normImage(loadGed('5.05_ID1662_769_0001.vol', ii));
     implantMask  = segmentImplant(im2);
     interestMask = (circ & ~implantMask);
@@ -75,13 +97,34 @@ for ii = 2:255
     boneMaskNextImg   = imerode(~mask3, seNextImg) & interestMask;  % why isn't the ~ on the cavityMaskNextImg?
     cavityMaskNextImg = imerode(mask3, seNextImg) & interestMask;
 
-    imsc(im2)
-    title(num2str(ii))
-    shadeArea(cavityMaskNextImg, [1 0 0])
-    drawnow
-    if ii > 60 && ii < 120
-        pause(1)
+    savedImplantMasks(:, :, ii) = implantMask;
+    savedBoneMasks(:, :, ii) = mask4;
+
+
+    %%  Do statistics
+    [xi, yi] = find(implantMask);
+    dstMap = sgnDstFromImg(implantMask);
+    % dstBorders = [dstMap(1:s, 1) dstMap(1:s, s) dstMap(1, 1:s)' dstMap(s, 1:s)'];
+    % maxRadius = floor(min(dstBorders));
+    % bandBorders = linspace(0, maxRadius, floor(maxRadius/nBands));
+    % boneVolume = zeros(nBands, 1);
+    % area = zeros(nBands, 1);
+
+    for jj = 1:nBands
+        dstMask = (dstMap < bandBorders(jj)) & (dstMap > 0);
+        boneVolume(jj) = sum(mask4(dstMask));
+        area(jj) = sum(dstMask(:));
+        % imsc(dstMask)
+        % pause(0.01)
     end
+
+
+
+
+    % imsc(im2)
+    % title(num2str(ii))
+    % shadeArea(mask4, [1 0 0])
+    % saveas(gcf,sprintf('segmentationOverlays/%3.3d.png', ii))
 
 
 %     currentFig = figure;
@@ -113,37 +156,14 @@ end
 
 % shadeLinker(im2, mask4, 'shadeAndMask')
 
-%%  De statistics
-% [xi, yi] = find(implantMask);
-% dstX = min(min(xi), s-max(xi));
-% dstY = min(min(yi), s-max(yi));
-% maxRadius = min(dstX, dstY);
-% % centroid = regionprops(implantMask, 'centroid');
-% % centroid = round(centroid.Centroid);
-% % maxRadius = min(abs([centroid centroid - s]));
-% % bandBorders = round(maxRadius/nBands : maxRadius/nBands : maxRadius*(1 - 1/nBands));
-% bandBorders = floor(maxRadius/nBands*(1:nBands));
-% boneVolumeFraction = zeros(1, nBands)*NaN;
-%
-%
-% dstMap = sgnDstFromImg(implantMask);
-% for ii = 1:nBands
-%     dstMask = (bandBorders(ii) > dstMap) & (dstMap > 0);
-%     if sum(sum(dstMask|circ)) > circArea
-%         warning('Circle outside image bounds for the %d''th iteration. Halting execution.', ii)
-%         break
-%     end
-%     boneVolumeFraction(ii) = sum(boneMask(dstMask))/(pi*bandBorders(ii)^2);% * 1/numel(dstMask);
-%     % imsc(dstMask)
-%     % pause(0.01)
-% end
-%
-% plot(boneVolumeFraction)
-%
-% % Check that there's no summing going on outside the circular image
-%
-% % Don't sum in the implant
-%
-% % Limut summing to first half of image
-%
-% % Reproduce Torstens graph
+
+
+plot(boneVolume)
+
+% Check that there's no summing going on outside the circular image
+
+% Don't sum in the implant
+
+% Limut summing to first half of image
+
+% Reproduce Torstens graph
