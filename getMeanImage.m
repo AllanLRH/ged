2319604c4d6f782496292387379ce682e9423a11
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-06-22 14:16:23 +0200

    Updated gitignore

--- getMeanImage.m
+++ getMeanImage.m
@@ -1,6 +1,5 @@
 function [meanImg] = getMeanImage(img, mask, boxsize)
 %% getMeanImage: Calculates the mean image by convolving with a box filter
     box = ones(boxsize)/boxsize^2;
-    meanImg = conv2(img, box, 'same') ./ conv2(double(mask), box, 'same');% .* mask;
-
+    meanImg = normConv(img, mask, box);
 end
