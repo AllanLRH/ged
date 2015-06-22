a3aa417b6d8f41d555b6c561a7457fc52511f840
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-06-22 14:22:50 +0200

    Align lines.

--- getMeanImage.m
+++ getMeanImage.m
@@ -1,5 +1,5 @@
 function [meanImg] = getMeanImage(img, mask, boxsize)
 %% getMeanImage: Calculates the mean image by convolving with a box filter
-    box = ones(boxsize)/boxsize^2;
+    box     = ones(boxsize)/boxsize^2;
     meanImg = normConv(img, mask, box);
 end
