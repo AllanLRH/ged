4075962c7ff9e567db88d72715224dad5b5b0744
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-06-23 14:00:43 +0200

    Added firstImageInStack.m

---
+++ firstImageInStack.m
@@ -0,0 +1,8 @@
+filenames = {'5.05_ID1662_770_0001.vol', '5.05_ID1662_771_0001.vol', '5.05_ID1662_772_0001.vol', ...
+             '5.05_ID1662_773_0001.vol', '5.05_ID1684_806_0001.vol', '5.05_ID1684_809_0001.vol', ...
+             '5.05_ID1689_805_0001.vol', '5.05_ID1689_807_0001.vol', '5.05_ID1689_808_0001.vol'};
+
+for ii = 1:length(filenames)
+    img = 255*normImage(loadGed(['data/' filenames{ii}], 1));
+    imwrite(img, ['firstImageInStack/' filenames{ii} '.tiff'])
+end
