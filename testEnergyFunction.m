c74013f09fba2620b030e7f4b2a541bed45cf5a5
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-06-29 21:01:25 +0200

    Renamed testEnergyFunctio to testEnergyImage

--- testEnergyFunction.m
+++
@@ -1,8 +0,0 @@
-% clear; home; close all
-
-im1 = normImage(loadGed('5.05_ID1662_769_0001.vol', 1));
-im2 = normImage(loadGed('5.05_ID1662_769_0001.vol', 2));
-
-ei = energyImage(im1, im2, 255);
-imsc(ei)
-% maximize
