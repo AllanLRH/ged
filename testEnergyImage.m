65441d17c1b30a67a92309b35a6429ffd2037ad8
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-06-29 21:26:05 +0200

    Chage search number of files to test and plot layout in testEnergyImage.m

--- testEnergyImage.m
+++ testEnergyImage.m
@@ -2,15 +2,16 @@ clear; home; close all
 
 im1 = normImage(loadGed('5.05_ID1662_769_0001.vol', 1));
 
+n = 30;
+
 entropySums = zeros([20, 1]);
 cnt = 1;
-ei = zeros(2048, 2048, 19);
-for ii = 2:20
+ei = zeros(2048, 2048, n-1);
+for ii = 2:n
     im2 = normImage(loadGed('5.05_ID1662_769_0001.vol', ii));
     ei(:, :, cnt) = energyImage(im1, im2, 60);
     entropySums(cnt) = sum(sum(ei(:,:,cnt)));
-    % im1 = im2;
     cnt = cnt + 1
 end
 
-plot(entropySums)
+plot(entropySums, '-o')
