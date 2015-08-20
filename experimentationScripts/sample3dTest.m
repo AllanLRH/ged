02dc2a8ce3dae995bec510c5fc42bfb0abbc1e72
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-08-20 10:48:58 +0200

    Added spacing around commas and .5 -> 0.5 in sample3dTest.m

--- experimentationScripts/sample3dTest.m
+++ experimentationScripts/sample3dTest.m
@@ -1,33 +1,38 @@
-N = 50;
+% N = 50;
 
+% N2 = round(N/2);
+% I = zeros(N, N, N);
+% I(N2+(-5:5), N2+(-10:10), N2+(-20:20)) = 1;
+
+I = loadGed('../data/5.05_ID1662_769_0001.vol', 1:255);
+N = size(I, 1);
 N2 = round(N/2);
-I = zeros(N,N,N);
-I(N2+(-5:5),N2+(-10:10),N2+(-20:20)) = 1;
-%w = [1,1,1]';
-w = randn(3,1);
-x = N2*ones(3,1);
-J = sample3d(I,x,[1,0,0]',w,(-(N2-1):N2),(-(N2-1):N2),(-(N2-1):N2));
+
+%w = [1, 1, 1]';
+w = randn(3, 1);
+x = N2*ones(3, 1);
+J = sample3d(I, x, [1, 0, 0]', w, (-(N2-1):N2), (-(N2-1):N2), (-(N2-1):N2));
 
 M = J>0.5;
-[w,x] = getMajorAxis(J>0.5);
-K = sample3d(J,x,[1,0,0]',w,-(N2-1):N2,-(N2-1):N2,10:30);
+[w, x] = getMajorAxis(J>0.5);
+K = sample3d(J, x, [1, 0, 0]', w, -(N2-1):N2, -(N2-1):N2, 10:30);
 
 figure(1);
-clf; isosurface(I,.5); axis equal, view(3)
+clf; isosurface(I, 0.5); axis equal, view(3)
 xlabel('x');
 ylabel('y');
 zlabel('z');
 
 
 figure(2);
-clf; isosurface(J,.5); axis equal, view(3)
+clf; isosurface(J, 0.5); axis equal, view(3)
 xlabel('x');
 ylabel('y');
 zlabel('z');
 
 figure(3);
-if(size(K,3)>1)
-    clf; isosurface(K,.5); axis equal, view(3)
+if(size(K, 3)>1)
+    clf; isosurface(K, 0.5); axis equal, view(3)
     xlabel('x');
     ylabel('y');
     zlabel('z');
@@ -37,7 +42,7 @@ else
     ylabel('y');
 end
 
-L = sample3d(I,x,[1,1,0]',w,(-(N2-1):N2),(-(N2-1):N2),0);
+L = sample3d(I, x, [1, 1, 0]', w, (-(N2-1):N2), (-(N2-1):N2), 0);
 clf; imagesc(L); axis equal, colormap(gray); colorbar;
 xlabel('x');
 ylabel('y');
