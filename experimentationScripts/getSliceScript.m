f52b56896f137df354606c15b361bf34ec99e92c
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-08-21 10:08:21 +0200

    Updated getSliceScript to use minified version of data; added minified dataset.

---
+++ experimentationScripts/getSliceScript.m
@@ -0,0 +1,53 @@
+clear; close; home
+
+% [x, y, z] = ndgrid(-2:.2:2, -2:.25:2, -2:.16:2);
+% volume = x.*exp(-x.^2 - y.^2 - z.^2);
+
+load('minivol.mat');
+volume = normImage(vol);
+xv = (1:size(vol, 1)) - size(vol, 1)/2;
+yv = (1:size(vol, 2)) - size(vol, 2)/2;
+zv = (1:size(vol, 3)) - size(vol, 3)/2;
+[x, y, z] = ndgrid(xv, yv, zv);
+
+planePoints = 128;  % points along one direction, will be squared
+angles = [20, 42, 68] * pi/180;
+
+minXYZ = min(min([x(:) y(:) z(:)]));
+maxXYZ = max(max([x(:) y(:) z(:)]));
+
+% Calculate the lowest and highest corners of the tilted plane
+planeMax = maxXYZ * (1 - min(cos(angles)));
+planeMin = minXYZ * (1 - min(cos(angles)));
+
+% Calculate start end end heights
+% (planeMax - planeMin) is the length of the slicing plane, and
+% it's divided by two because it rotated around the center.
+% max(sin(angles)) is the (maximum) displacement of the corners of the
+% plane, caused by the rotation.
+startHeight = maxXYZ + (planeMax - planeMin)*max(sin(angles))/2;
+endHeight =   minXYZ - (planeMax - planeMin)*max(sin(angles))/2;
+
+figure
+rotMat = getRotMat(angles);
+% Make plane points for each iteration
+for k = startHeight:-1:endHeight
+    xax = linspace(floor(planeMin), ceil(planeMax), planePoints);
+    yax = linspace(floor(planeMin), ceil(planeMax), planePoints);
+    [xd, yd] = meshgrid(xax, yax);
+    zd = zeros(planePoints) + k;
+    % Make vectors from plane points, and apply rotation matrix
+    for ii = 1:numel(xd)
+        vec = [xd(ii); yd(ii); zd(ii)];
+        vec = rotMat*vec;
+        xd(ii) = vec(1);
+        yd(ii) = vec(2);
+        zd(ii) = vec(3);
+    end
+
+    imslice = interpn(x, y, z, volume, xd, yd, zd);
+    imsc(imslice)
+    colorbar
+    caxis([0 1])
+    drawnow
+end
