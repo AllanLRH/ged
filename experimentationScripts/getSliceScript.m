5cf2625af29e66ac1718f279be4fb15104a4f940
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-08-30 14:40:49 +0200

    getSliceScipt uses minified dataset.

--- experimentationScripts/getSliceScript.m
+++ experimentationScripts/getSliceScript.m
@@ -3,14 +3,19 @@ clear; close; home
 % [x, y, z] = ndgrid(-2:.2:2, -2:.25:2, -2:.16:2);
 % volume = x.*exp(-x.^2 - y.^2 - z.^2);
 
-load('minivol.mat');
-volume = normImage(vol);
+% load('minivol.mat');
+% volume = normImage(vol);
+
+volume = normImage(makeCircle(128, 40, [64 64]) - makeCircle(128, 45, [64 64]));
+volume = repmat(volume, 1, 1, 85);
+vol = volume;
+
 xv = (1:size(vol, 1)) - size(vol, 1)/2;
 yv = (1:size(vol, 2)) - size(vol, 2)/2;
 zv = (1:size(vol, 3)) - size(vol, 3)/2;
 [x, y, z] = ndgrid(xv, yv, zv);
 
-planePoints = 128;  % points along one direction, will be squared
+planePoints = 256;  % points along one direction, will be squared
 angles = [20, 42, 68] * pi/180;
 
 minXYZ = min(min([x(:) y(:) z(:)]));
@@ -32,8 +37,10 @@ figure
 rotMat = getRotMat(angles);
 % Make plane points for each iteration
 for k = startHeight:-1:endHeight
-    xax = linspace(floor(planeMin), ceil(planeMax), planePoints);
-    yax = linspace(floor(planeMin), ceil(planeMax), planePoints);
+    % xax = linspace(floor(2*planeMin), ceil(0.5*planeMax), planePoints);
+    % yax = linspace(floor(2*planeMin), ceil(0.5*planeMax), planePoints);
+    xax = linspace(-128, 128, planePoints);
+    yax = linspace(-128, 128, planePoints);
     [xd, yd] = meshgrid(xax, yax);
     zd = zeros(planePoints) + k;
     % Make vectors from plane points, and apply rotation matrix
