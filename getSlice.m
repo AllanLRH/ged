85d64215fefd1368dc5f3620891a0ac618635878
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-09-29 16:04:32 +0200

    Added getSlice.m and getSliceTest.m. Function seems to be working

--- getSlice.m
+++ getSlice.m
@@ -1,25 +1,22 @@
-function imgSlice = getSlice(vol, angles, height, planePoints)
-    xax = linspace(-size(vol, 1)/sqrt(2), size(vol, 1)/sqrt(2), planePoints);
-    yax = linspace(-size(vol, 2)/sqrt(2), size(vol, 2)/sqrt(2), planePoints);
-    rotMat = getRotMat(angles);
-
-    xv = (1:size(vol, 1)) - size(vol, 1)/2;
-    yv = (1:size(vol, 2)) - size(vol, 2)/2;
-    zv = (1:size(vol, 3)) - size(vol, 3)/2;
-    [x, y, z] = ndgrid(xv, yv, zv);
-
-    [xd, yd] = meshgrid(xax, yax);
-    zd = zeros(planePoints) + height;
-    for ii = 1:numel(zd)
-        vec = [xd(ii); yd(ii); zd(ii)];
-        % vec = rotMat*vec;
-        vec = [rotMat(1,1)*vec(1) + rotMat(1,2)*vec(2) + rotMat(1,3)*vec(3)
-               rotMat(2,1)*vec(1) + rotMat(2,2)*vec(2) + rotMat(2,3)*vec(3)
-               rotMat(3,1)*vec(1) + rotMat(3,2)*vec(2) + rotMat(3,3)*vec(3)];
-        xd(ii) = vec(1);
-        yd(ii) = vec(2);
-        zd(ii) = vec(3);
-    end
-    imgSlice = interpn(x, y, z, vol, xd, yd, zd);
-end  % getslice
-
+function imgSlice = getSlice(vol, rotateDirection, angle, height, planePoints)
+    % Inspired by example two in http://se.mathworks.com/help/matlab/ref/slice.html
+    [x, y, z] = meshgrid(linspace(-1, 1, size(vol, 1)), linspace(-1, 1, size(vol, 2)), linspace(-1, 1, size(vol, 3)));
+    hsp = surf(linspace(-1, 1, planePoints), linspace(-1, 1, planePoints), zeros(planePoints) + 2*height-1);
+    upperLim = 1;
+    lowerLim = -1*upperLim;
+    xlim([lowerLim upperLim]);
+    ylim([lowerLim upperLim]);
+    zlim([lowerLim upperLim]);
+    rotate(hsp, rotateDirection, angle);
+    xd = hsp.XData;
+    yd = hsp.YData;
+    zd = hsp.ZData;
+    shading flat
+    xlim([lowerLim upperLim]);
+    ylim([lowerLim upperLim]);
+    zlim([lowerLim upperLim]);
+    view(3)
+    xlabel(1); ylabel(2); zlabel(3);
+    s = slice(x, y, z, vol, xd, yd, zd);
+    imgSlice = s.CData;
+end
