0644aeae23c0d7fccba9ba300f681f4f42b5d299
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-10-30 08:23:01 +0100

    boneFraction.m used for computing segmentation

--- boneFraction.m
+++ boneFraction.m
@@ -1,18 +1,27 @@
 clear; %close all; clc
 %%
 
+timeRightNow = @() sprintf('%s:    ', datestr(datetime('now')));
+fid = fopen('logfile_boneFraction.txt', 'a');
+
 nBands = 100;
 stackSize = 1000;
 boxsize = 5;
 load('circ.mat')
 circArea     = sum(circ(:));
 
+boneVolume  = NaN*ones(stackSize, nBands);
+area        = NaN*ones(stackSize, nBands);
+
 fileCell = {'5.05_ID1662_769_0001.vol', '5.05_ID1662_770_0001.vol', '5.05_ID1662_771_0001.vol',...
             '5.05_ID1662_772_0001.vol', '5.05_ID1662_773_0001.vol', '5.05_ID1684_806_0001.vol',...
             '5.05_ID1684_809_0001.vol', '5.05_ID1689_805_0001.vol', '5.05_ID1689_807_0001.vol',...
             '5.05_ID1689_808_0001.vol'};
-
+%%
 for fn = 1:length(fileCell)
+    msg = sprintf('%sProcessing dataset containing file %s\n', timeRightNow(), fileCell{fn});
+    fprintf(msg);
+    fprintf(fid, msg);
     dataset = ['data/' fileCell{fn}];
     im1 = normImage(loadDataset(dataset, 1));
     s = size(im1, 1);  %  square image dimmensions
@@ -28,7 +37,9 @@ for fn = 1:length(fileCell)
     im1          = im1 - bias;
 
     % Load image where the initial bone and cavity areas are marked. Ignore granulate channel for now
-    temp          = logical(imread('firstImageInStack/5.05_ID1662_769_0001_mask.vol.png'));
+    tempName = fileCell{fn};
+    tempName = tempName(1:(length(tempName)-4));
+    temp          = logical(imread(['firstImageInStack/' tempName '_mask.vol.png']));
     boneMask      = temp(:,:,3);
     cavityMask    = temp(:,:,1);
     % granulateMask = temp(:,:,2);
@@ -62,50 +73,50 @@ for fn = 1:length(fileCell)
     boneMaskNextImg   = imerode(~mask1, seNextImg) & interestMask;  % why isn't the ~ on the cavityMaskNextImg?
     cavityMaskNextImg = imerode(mask1, seNextImg) & interestMask;
 
-    [xi, yi] = find(implantMask);
-    dstMap = sgnDstFromImg(implantMask);
-    dstBorders = [dstMap(1:s, 1) dstMap(1:s, s) dstMap(1, 1:s)' dstMap(s, 1:s)'];
-    maxRadius = floor(min(dstBorders(:)));
-    bandBorders = linspace(0, maxRadius, nBands);
-    boneVolume = zeros(nBands, 1);
-    area = zeros(nBands, 1);
-    boneCavityFraction = zeros(stackSize, 1);
-    tempBoneMask = (~mask1 & interestMask);
-    tempCavityMask = (mask1 & interestMask);
+    % Do statistics
+    dstMap                = sgnDstFromImg(implantMask);
+    dstBorders            = [dstMap(1:s, 1) dstMap(1:s, s) dstMap(1, 1:s)' dstMap(s, 1:s)'];
+    maxRadius             = floor(min(dstBorders(:)));
+    bandBorders           = linspace(0, maxRadius, nBands);
+    tempBoneMask          = (~mask1 & interestMask);
+    tempCavityMask        = (mask1 & interestMask);
+    boneCavityFraction    = NaN*ones(stackSize, 1);
     boneCavityFraction(1) = sum(tempBoneMask(:))/sum(tempCavityMask(:));
 
     for ii = 1:nBands
         dstMask = (dstMap < bandBorders(ii)) & (dstMap > 0);
-        boneVolume(ii) = sum(boneMask(dstMask));
-        area(ii) = sum(dstMask(:));
-        % imsc(dstMask)
-        % pause(0.01)
+        boneVolume(fn, ii) = sum(boneMask(dstMask));
+        area(fn, ii) = sum(dstMask(:));
     end
 
     %% Process next image
     for ii = 2:stackSize
-        disp(ii/stackSize*100)
+        if mod(ii, 50) == 0
+            msg = sprintf('%sProcessing image %d of %d in stack\n', timeRightNow(), ii, stackSize);
+            fprintf(msg)
+            fprintf(fid, msg)
+        end
         im2 = normImage(loadDataset('5.05_ID1662_769_0001.vol', ii));
         implantMask  = segmentImplant(im2);
         interestMask = (circ & ~implantMask);
         bias         = biasCorrect(im2, interestMask);
         im2          = im2 - bias;
 
-        boneMean   = median(im2(boneMaskNextImg));
-        boneStd    = median(abs(im2(boneMaskNextImg)-boneMean));
-        %cavityStd  = std(im2(cavityMaskNextImg));
-        %cavityMean = mean(im2(cavityMaskNextImg));
+        boneMean     = median(im2(boneMaskNextImg));
+        boneStd      = median(abs(im2(boneMaskNextImg)-boneMean));
+        cavityStd    = std(im2(cavityMaskNextImg));
+        cavityMean   = mean(im2(cavityMaskNextImg));
         % Using median for a "mean-ness" measure, becaus of it's robustness for
         % (unskewed) noise and certainly from "hot pixels" (Jon's proposal)
-        cavityMean   = median(im2(cavityMaskNextImg));
-        cavityStd    = median(abs(im2(cavityMaskNextImg)-cavityMean));
+        % cavityMean = median(im2(cavityMaskNextImg));
+        % cavityStd  = median(abs(im2(cavityMaskNextImg)-cavityMean));
 
-        meanImg = getMeanImage(im2, interestMask, boxsize);
-        stdImg  = getVarImage(im2, interestMask, boxsize, meanImg);
-        bone2   = (meanImg-boneMean).^2+(stdImg-boneStd).^2;
-        cavity2 = (meanImg-cavityMean).^2+(stdImg-cavityStd).^2;
-        mask3   = (bone2 > cavity2);
-        boneMask   = imclose(mask3, seCleaner) & interestMask;
+        meanImg  = getMeanImage(im2, interestMask, boxsize);
+        stdImg   = getVarImage(im2, interestMask, boxsize, meanImg);
+        bone2    = (meanImg-boneMean).^2+(stdImg-boneStd).^2;
+        cavity2  = (meanImg-cavityMean).^2+(stdImg-cavityStd).^2;
+        mask3    = (bone2 > cavity2);
+        boneMask = imclose(mask3, seCleaner) & interestMask;
 
         boneMaskNextImg   = imerode(~mask3, seNextImg) & interestMask;  % why isn't the ~ on the cavityMaskNextImg?
         cavityMaskNextImg = imerode(mask3, seNextImg) & interestMask;
@@ -118,28 +129,27 @@ for fn = 1:length(fileCell)
         boneCavityFraction(ii) = sum(tempBoneMask(:))/sum(tempCavityMask(:));
 
         %%  Do statistics
-        % [xi, yi] = find(implantMask);
-        dstMap = sgnDstFromImg(implantMask);
-        dstBorders = [dstMap(1:s, 1) dstMap(1:s, s) dstMap(1, 1:s)' dstMap(s, 1:s)'];
-        maxRadius = max(floor(min(dstBorders)));
+        dstMap        = sgnDstFromImg(implantMask);
+        dstBorders    = [dstMap(1:s, 1) dstMap(1:s, s) dstMap(1, 1:s)' dstMap(s, 1:s)'];
+        maxRadius     = floor(min(dstBorders(:)));
         % bandBorders = linspace(0, maxRadius, floor(maxRadius/nBands));
-        bandBorders = linspace(0, maxRadius, nBands);
-        boneVolume = NaN*ones(nBands, 1);
-        area = NaN*ones(nBands, 1);
+        bandBorders   = linspace(0, maxRadius, nBands);
+        boneVolume    = NaN*ones(nBands, 1);
+        area          = NaN*ones(nBands, 1);
 
 
         for jj = 1:nBands
             dstMask = (dstMap < bandBorders(jj)) & (dstMap > 0);
-            boneVolume(jj) = sum(boneMask(dstMask));
-            area(jj) = sum(dstMask(:));
-            % imsc(dstMask)
-            % pause(0.01)
+            boneVolume(fn, jj) = sum(boneMask(dstMask));
+            area(fn, jj) = sum(dstMask(:));
         end
-        csvwrite(sprintf('boneVolume_%d.csv', ii), boneVolume)
-        csvwrite(sprintf('area_%d.csv', ii), area)
-        save(['../segmentations/' dataset '_v6_double.mat'], 'savedImplantMasks', 'savedBoneMasks', '-v6')
-    end
 
-    plot(boneCavityFraction, 'o-');
-    saveas(gca, 'boneCavityFraction.eps', 'epsc');
     end
+    datasetName = fileCell{fn};
+    datasetName = datasetName(1:(length(datasetName)-9));
+    save(['segmentations/' datasetName '_double.mat'], 'savedImplantMasks', 'savedBoneMasks', '-v7.3')
+    save(['stats/' datasetName '_double.mat'], 'boneVolume', 'area', '-v7.3')
+
+    % plot(boneCavityFraction, 'o-');
+    % saveas(gca, 'boneCavityFraction.eps', 'epsc');
+end
