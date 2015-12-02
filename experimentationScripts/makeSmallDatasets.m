8ea2d367e6defa2982f22dcc553bc4922368b9da
Allan L. R. Hansen <allan.lrh@gmail.com>
2015-12-02 13:22:04 +0100

    Fixed makeSmallDatasets.m

--- experimentationScripts/makeSmallDatasets.m
+++ experimentationScripts/makeSmallDatasets.m
@@ -3,28 +3,30 @@ clear; home; close all;
 fileGroups = {{'../data/5.05_ID1662_769_0001.vol', '../data/5.05_ID1662_769_0002.vol', '../data/5.05_ID1662_769_0003.vol', '../data/5.05_ID1662_769_0004.vol'}, {'../data/5.05_ID1662_770_0001.vol', '../data/5.05_ID1662_770_0002.vol', '../data/5.05_ID1662_770_0003.vol', '../data/5.05_ID1662_770_0004.vol'}, {'../data/5.05_ID1662_771_0001.vol', '../data/5.05_ID1662_771_0002.vol', '../data/5.05_ID1662_771_0003.vol', '../data/5.05_ID1662_771_0004.vol'}, {'../data/5.05_ID1662_772_0001.vol', '../data/5.05_ID1662_772_0002.vol', '../data/5.05_ID1662_772_0003.vol', '../data/5.05_ID1662_772_0004.vol'}, {'../data/5.05_ID1662_773_0001.vol', '../data/5.05_ID1662_773_0002.vol', '../data/5.05_ID1662_773_0003.vol', '../data/5.05_ID1662_773_0004.vol'}, {'../data/5.05_ID1684_806_0001.vol', '../data/5.05_ID1684_806_0002.vol', '../data/5.05_ID1684_806_0003.vol', '../data/5.05_ID1684_806_0004.vol'}, {'../data/5.05_ID1684_809_0001.vol', '../data/5.05_ID1684_809_0002.vol', '../data/5.05_ID1684_809_0003.vol', '../data/5.05_ID1684_809_0004.vol'}, {'../data/5.05_ID1689_805_0001.vol', '../data/5.05_ID1689_805_0002.vol', '../data/5.05_ID1689_805_0003.vol', '../data/5.05_ID1689_805_0004.vol'}, {'../data/5.05_ID1689_807_0001.vol', '../data/5.05_ID1689_807_0002.vol', '../data/5.05_ID1689_807_0003.vol', '../data/5.05_ID1689_807_0004.vol'}, {'../data/5.05_ID1689_808_0001.vol', '../data/5.05_ID1689_808_0002.vol', '../data/5.05_ID1689_808_0003.vol', '../data/5.05_ID1689_808_0004.vol'}};
 
 for ii = 1:length(fileGroups)
-    disp('ii')
-    disp(ii)
+    fprintf('\nii: %d\n', ii)
     group = fileGroups{ii};
     endData = parseVolInfo(group{end});
     numZ = endData.NUM_Z;
-    vol = NaN*ones(2048, 2048, (256*(length(group) - 1) + numZ));
-    for jj = 1:(length(group)-1)
-        disp('jj')
-        disp(jj)
-        tempVol = loadGed(group{jj}, 1:256);
-        % d(:, :, 1:size(b,3)) = b
-        vol(:, :, (256*(jj-1)+1) : (256*jj)) = tempVol;
-    end
-    vol(:, :, (jj*256+1):size(vol,3)) = loadGed(group{jj+1}, 1:numZ);
+    vol = NaN*ones(2048, 2048, 256*4+numZ);
+    group = fileGroups{ii};
+    fname = group{1};
+    vol(:, :, 1:256) = normImage(loadGed(fname));
+    fname = group{2};
+    vol(:, :, 257:513) = normImage(loadGed(fname));
+    fname = group{3};
+    vol(:, :, 514:770) = normImage(loadGed(fname));
+    fname = group{4};
+    vol(:, :, 771:(771+numZ)) = normImage(loadGed(fname));
     if any(isnan(vol))
         error('vol contains %d NaNs\n', sum(isnan(vol)))
     end
-    newVol = imresize(vol, 1/4);
-    name = group{1}
+    for jj = 1:4:size(vol, 3)
+        newVol = imresize(vol(:,:,jj), 1/4);
+    end
+
     save(['../smallData/' name(9:end-9) '_v6_double.mat'], 'newVol', '-v6');
     save(['../smallData/' name(9:end-9) '_v7.3_double.mat'], 'newVol', '-v7.3');
-    newVol = uint8(256*normImage(newVol));
+    newVol = uint8(256*newVol);
     save(['../smallData/' name(9:end-9) '_v6_uint8.mat'], 'newVol', '-v6');
     save(['../smallData/' name(9:end-9) '_v7.3_uint8.mat'], 'newVol', '-v7.3');
 end
