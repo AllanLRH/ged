5b94d1d1b13fbce72b43d7350c8032ce9a43cd6d
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-01-07 13:19:18 +0100

    Updated plotShadingPlay to handle reversed input order

--- experimentationScripts/plotShadingPlay.m
+++ experimentationScripts/plotShadingPlay.m
@@ -3,8 +3,14 @@ clear; close all; home
 im = rgb2gray(imread('../769hele implantatetx1.tif'));
 imsc(im)
 % [x, y] = ginput(2);
-x = [776.08 3587];
-y = [944.24 1536.5];
+% x = [776.08 3587];
+% y = [944.24 1536.5];
+x = [3587 776.08];
+y = [1536.5 944.24];
+% Sort points, upperleft first.
+% NOTE: Ensure that it's enough to check the length!
+[~, i] = sort([sqrt(x(1)^2 + y(1)^2) sqrt(x(2)^2 + y(2)^2)]);
+x = x(i); y = y(i);
 hold on
 for ii = 1:2
     plot(x(ii), y(ii), 'ro')
@@ -23,5 +29,4 @@ maskY2 = y(2) < r;
 
 maskFinal = maskX1 | maskX2 | maskY1 | maskY2;
 shadeArea(maskFinal, [1 0 0])
-
 
