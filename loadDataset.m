4aea062c580985514f7150874c19aa9830c70552
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-01-07 10:59:49 +0100

    Corrected bug and added comments to loadDataset.m

--- loadDataset.m
+++ loadDataset.m
@@ -3,21 +3,21 @@
 % datafile: A datafile from the dataset
 % slice: A vector with slice indices to be returned
 function out = loadDataset(datafile, slice)
-    fileGroup = getFileGroup(datafile);
+    fileGroup = getFileGroup(datafile);  % names of datafiles in dataset (cell)
     fileGroupInfo = cellfun(@parseVolInfo, fileGroup);
     x = unique(cell2mat({fileGroupInfo.NUM_X}));
     y = unique(cell2mat({fileGroupInfo.NUM_Y}));
     maxZ = max(cell2mat({fileGroupInfo.NUM_Z}));
     sumZ = sum(cell2mat({fileGroupInfo.NUM_Z}));
-    if any(slice > sumZ)
+    if any(slice > sumZ)  % check that requested slices is in data
         idx = find(slice > sumZ);
         error('Slice %d not in stack containing datafile %s', idx, datafile);
     end
-    fileMap = ceil(slice/maxZ);
-    out = NaN(x, y, length(slice));
+    fileMap = ceil(slice/maxZ);  % map each slice to a file from fileGroup by index
+    out = NaN(x, y, length(slice));  % preallocate output
     cnt = 1;
     for sl = slice-((fileMap-1)*maxZ)
-        filename = fileGroup{fileMap};
+        filename = fileGroup{fileMap(cnt)};  % set filename for current slices
         out(:, :, cnt) = loadGed(filename, sl);
         cnt = cnt + 1;
     end
