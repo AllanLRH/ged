c98d9c7d9fe9bac2f850fa3447bc0656b74a219e
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-01-27 15:49:23 +0100

    Added more progress printing and corrected brackets in makeSmallDatasets

--- experimentationScripts/makeSmallDatasets.m
+++ experimentationScripts/makeSmallDatasets.m
@@ -14,19 +14,22 @@ for ii = 1:length(fileGroups)
     groupInfo = cellfun(@parseVolInfo, group);
     numZ = sum(cell2mat({groupInfo.NUM_Z}));  % Number of slices in dataset
     fname = group{1};
+    fprintf('\tLoading dataset\n')
     vol = loadDataset(fullfile(volDir, fname), 1:numZ);
     % Example: nameIdPart of "volfloat/5.05_ID1662_769_pag0001.vol" is "5.05_ID1662_769_pag".
     nameIdPart = regexpi(fname(10:end), pathSep, 'split'); nameIdPart = nameIdPart{end};
     nameIdPart = regexpi(nameIdPart, '([\d.]+_)?ID\d+_\d+_[a-zA-Z]*', 'match');
     for sf = scaleFactorMat
+        fprintf('\tScaling dataset with scale factor %d\n', sf)
         [x1q, x2q, x3q] = ndgrid(linspace(1, size(vol, 1), size(vol, 1)/sf), ...
                                  linspace(1, size(vol, 2), size(vol, 2)/sf), ...
                                  linspace(1, size(vol, 3), size(vol, 3)/sf));
         newVol = interpn(vol, x1q, x2q, x3q);
+        fprintf('\tSaving dataset\n')
         if sf == 4
-            save(char(fullfile('..', 'smallData', nameIdPart, '_v7.3_double.mat')), 'newVol', '-v7.3');
+            save(char(fullfile('..', 'smallData', [nameIdPart '_v7.3_double.mat'])), 'newVol', '-v7.3');
         elseif sf == 2
-            save(char(fullfile('..', 'halfSizeData', nameIdPart, '_v7.3_double.mat')), 'newVol', '-v7.3');
+            save(char(fullfile('..', 'halfSizeData', [nameIdPart '_v7.3_double.mat'])), 'newVol', '-v7.3');
         end
     end  % for scaleFactor
     toc
