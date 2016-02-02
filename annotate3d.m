07c3ee93721285691be2ce77c929d3b12ff7c607
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-02-02 15:08:41 +0100

    annotate3d now uses struct to save data (untested)

--- annotate3d.m
+++ annotate3d.m
@@ -1,4 +1,4 @@
-inPath = '../gedData/smallData/';
+inPath = 'smallData/';
 outPath = '';
 avoidEdgeDistance = 10;
 minSlice = 1;
@@ -10,49 +10,64 @@ maxDistance = 100;
 SHOWRESULT = true;
 SAVERESULT = true;
 
-filenames= {'5.05_ID1662_769_v7.3_double', ...
-    '5.05_ID1662_770_v7.3_double', ...
-    '5.05_ID1662_771_v7.3_double', ...
-    '5.05_ID1662_772_v7.3_double', ...
-    '5.05_ID1662_773_v7.3_double', ...
-    '5.05_ID1684_806_v7.3_double', ...
-    '5.05_ID1684_809_v7.3_double', ...
-    '5.05_ID1689_805_v7.3_double', ...
-    '5.05_ID1689_807_v7.3_double', ...
-    '5.05_ID1689_808_v7.3_double'};
+% filenames= {'5.05_ID1662_769_v7.3_double', ...
+%     '5.05_ID1662_770_v7.3_double', ...
+%     '5.05_ID1662_771_v7.3_double', ...
+%     '5.05_ID1662_772_v7.3_double', ...
+%     '5.05_ID1662_773_v7.3_double', ...
+%     '5.05_ID1684_806_v7.3_double', ...
+%     '5.05_ID1684_809_v7.3_double', ...
+%     '5.05_ID1689_805_v7.3_double', ...
+%     '5.05_ID1689_807_v7.3_double', ...
+%     '5.05_ID1689_808_v7.3_double'};
+
+filenames = {'5.05_ID1798_776_pag_v7.3_double.mat', ...
+'5.05_ID1798_777_pag_v7.3_double.mat', '5.05_ID1798_778_pag_v7.3_double.mat', ...
+'5.05_ID1886_810b_pag_v7.3_double.mat', '5.05_ID1886_811b_pag_v7.3_double.mat', ...
+'5.05_ID1886_813_pag_v7.3_double.mat', '5.05_ID1886_814b_pag_v7.3_double.mat', ...
+'5.05_ID5597_780_pag_v7.3_double.mat', '5.05_ID5597_781_pag_v7.3_double.mat', ...
+'5.05_ID5597_783_pag_v7.3_double.mat', '5.05_ID5598_784_pag_v7.3_double.mat', ...
+'5.05_ID5598_785_pag_v7.3_double.mat', '5.05_ID5598_786_pag_v7.3_double.mat', ...
+'5.05_ID5598_787_pag_v7.3_double.mat', '5.05_ID5598_788_pag_v7.3_double.mat'}
 
 
 p = cell(length(filenames),17);
 figure(1); clf;
-for i = 1:length(filenames)
+% load('annotations.mat')
+for i = 9:length(filenames)
+    nameID = regexpi(filenames{i}, 'ID.+?(?=_v[\d.]+)', 'match');
     disp(filenames{i});
-    inputFilename = [inPath,filenames{i},'.mat'];
-    load(inputFilename);
+    p.(nameID).inputFilename = [inPath gfilenames{i}];
+    load(p.(nameID).inputFilename);
     showSlice = round(size(newVol,3)/2);
-    imagesc(newVol(:,:,showSlice)); title(sprintf('Bias corrected slice %d',showSlice)); colormap(gray); axis image tight;
-    
+    imagesc(newVol(:,:,showSlice)); title(sprintf('Bias corrected slice %d', showSlice)); colormap(gray); axis image tight;
+
     title('Select Bone');
     x = ginput(1);
-    boneExample = round([x(2),x(1),showSlice]);
+    p.(nameID).aBoneExample = round([x(2),x(1),showSlice]);
     title('Select Cavity');
     x = ginput(1);
-    cavityExample = round([x(2),x(1),showSlice]);
+    p.(nameID).aCavityExample = round([x(2),x(1),showSlice]);
     title('Select Implant');
     x = ginput(1);
-    implantExample = round([x(2),x(1),showSlice]);
-    
-    implantThreshold = (newVol(implantExample(1),implantExample(2),implantExample(3))+newVol(boneExample(1),boneExample(2),boneExample(3)))/2;
+    p.(nameID).anImplantExample = round([x(2),x(1),showSlice]);
+
+    implantThreshold = (newVol(p.(nameID).anImplantExample(1), ...
+                        p.(nameID).anImplantExample(2), ...
+                        p.(nameID).anImplantExample(3)) + newVol(p.(nameID).aBoneExample(1), ...
+                        p.(nameID).aBoneExample(2), ...
+                        p.(nameID).aBoneExample(3)))/2;
     implant = segmentImplant3d(newVol, implantThreshold);
-    
+
     xMax = round(size(implant)/2);
     x1 = 0;
     x2 = -(xMax(2)-1):xMax(2);
     x3 = -(xMax(3)-1):xMax(3);
     accepted = false;
-    [origo,R,~] = getAxes3d(implant);
+    [p.(nameID).origo, p.(nameID).R, ~] = getAxes3d(implant);
     while ~accepted
         slice = squeeze(sample3d(newVol,origo,R,x1,x2,x3));
-        
+
         imagesc(slice); title('Saggital slice'); colormap(gray); axis image tight;
         marks = zeros(4,3);
         for j = 1:size(marks,1)
@@ -60,6 +75,7 @@ for i = 1:length(filenames)
             x = ginput(1);
             marks(j,:) = [x(2),showSlice,x(1)];
         end
+        p.(nameID).marks = marks;
         v = marks(end,:)-marks(1,:);
         v = v'/sqrt(sum(v.^2));
         %disp(abs(dot(v,[0,0,1])));
@@ -75,7 +91,16 @@ for i = 1:length(filenames)
             R(:,1) = R(:,1)/sqrt(sum(R(:,1).^2));
         end
     end
-    p(i,:) = {inputFilename,boneExample,cavityExample,implantExample,avoidEdgeDistance, minSlice, maxSlice, halfEdgeSize, filterRadius, maxIter, maxDistance, SHOWRESULT, SAVERESULT, [outPath,p{i,1}], origo, R, marks};
+    p.(nameID).size = size(newVol);
+    p.(nameID).avoidEdgeDistance;
+    p.(nameID).minSlice;
+    p.(nameID).maxSlice;
+    p.(nameID).halfEdgeSize;
+    p.(nameID).filterRadius;
+    p.(nameID).maxIter;
+    p.(nameID).maxDistance;
+    p.(nameID).SHOWRESULT;
+    p.(nameID).SAVERESULT;
 end
 
-save([outPath,'annotations.mat'],'p');
+save([outPath,'annotations3.mat'], 'p');
