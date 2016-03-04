86823759bb6fc43eab7ceb8b3cd3ef82cb8643ea
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-03-04 16:21:30 +0100

    Jon's changes to makeTex3d.m

--- maketex3d.m
+++ maketex3d.m
@@ -1,19 +1,20 @@
-clear
+function maketex3d()
+
 SMALLDATA = true;
 
 % Visualization parameters
 numberSlicesToShow = 3; % The number of exemplar slices generated
 
 % Prefixes for the data files
-annotationsPrefix = fullfile('~', 'ged'); % Annotation file prefix (input)
+annotationsPrefix = fullfile('.'); % Annotation file prefix (input)
 if SMALLDATA
-    analysisPrefix = fullfile('~', 'ged', 'smallData'); % Analysis files prefix (input)
-    pdfPrefix = fullfile('~', 'gedTex', 'figuresSmall'); % pdf filename prefix (output)
+    analysisPrefix = fullfile('smallData'); % Analysis files prefix (input)
+    pdfPrefix = fullfile('..', 'gedTex', 'figuresSmall'); % pdf filename prefix (output)
 else
-    analysisPrefix = fullfile('~', 'ged', 'halfSizeData'); % Analysis files prefix (input)
-    pdfPrefix = fullfile('~', 'gedTex', 'figuresMedium'); % pdf filename prefix (output)
+    analysisPrefix = fullfile('halfSizeData'); % Analysis files prefix (input)
+    pdfPrefix = fullfile('..', 'gedTex', 'figuresMedium'); % pdf filename prefix (output)
 end
-latexFile = fullfile('~', 'gedTex', 'autoMain.tex'); % pdf filename prefix (output)
+latexFile = fullfile('..', 'gedTex', 'autoMain.tex'); % pdf filename prefix (output)
 
 %-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%-%
 fid = fopen(latexFile, 'wt');
@@ -28,122 +29,74 @@ for j = 1:length(names)
     [fp, fn, fe] = fileparts(filePath);
     % Escape underscores in filename, so that it may be used in a LaTeX caption
     fnEsc = strrep(fn, '_', '\_');
-
+    
     newVolSize = whos('-file', filePath); newVolSize = newVolSize.size;
     slices = round(linspace(1, newVolSize(3), numberSlicesToShow+2));
     slices = slices(2:end-1);
-    fprintf(fid, '\\begin{figure}\n  \\centering\n');
-    for i = slices
-        % make filename path and check that the file exists
-        filename = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'original_slice', i));
-        if exist([filename '.pdf'], 'file') == 0
-            warning('Filename "%s.pdf" does not exist!', filename)
-            missingFiles{length(missingFiles)+1} = [filename '.pdf'];
-        end
-        % Write the line to the tex file
-        fprintf(fid, '  \\subfigure[z=%d voxels]{\\includegraphics[width=0.3\\linewidth]{%s}}\n', i, filename);
-    end
-    fprintf(fid, '  \\caption{%s: Original image sections.}  \n  \\label{fig:%s}\n\\end{figure}\n', fnEsc, fn);
 
-    fprintf(fid, '\\begin{figure}\n  \\centering\n');
-    % make filename path and check that the file exists
-    filename = fullfile(pdfPrefix, sprintf('%s_%s', fn, 'implant'));
-    if exist([filename '.png'], 'file') == 0
-        warning('Filename "%s.png" does not exist!', filename)
-        missingFiles{length(missingFiles)+1} = [filename '.png'];
-    end
-    % Write the line to the tex file
-    fprintf(fid, '  \\subfigure{\\includegraphics[width=0.3\\linewidth]{%s}}\n', filename);
-    % make filename path and check that the file exists
-    filename = fullfile(pdfPrefix, sprintf('%s_%s', fn, 'zones'));
-    if exist([filename '.pdf'], 'file') == 0
-        warning('Filename "%s.pdf" does not exist!', filename)
-        missingFiles{length(missingFiles)+1} = [filename '.pdf'];
-    end
-    % Write the line to the tex file
-    fprintf(fid, '  \\subfigure{\\includegraphics[width=0.3\\linewidth]{%s}}\n', filename);
-    fprintf(fid, '  \\caption{%s: Implant and zones.}\n  \\label{fig:%s}\n\\end{figure}\n', fnEsc, fn);
-
-    fprintf(fid, '\\begin{figure}\n  \\centering\n');
+    fprintf(fid, '\\section{%s}\n',fnEsc);
+    fprintf(fid, '\\begin{figure}[H]\n  \\centering\n');
     for i = slices
-        % make filename path and check that the file exists
-        filename = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'bias_corrected_slice', i));
-        if exist([filename '.pdf'], 'file') == 0
-            warning('Filename "%s.pdf" does not exist!', filename)
-            missingFiles{length(missingFiles)+1} = [filename '.pdf'];
-        end
-        % Write the line to the tex file
-        fprintf(fid, '  \\subfigure{\\includegraphics[width=0.3\\linewidth]{%s}}\n', filename);
+        prefix = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'original_slice', i));
+        missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, '', missingFiles);
+    end
+    for i = slices
+        prefix = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'bias_corrected_slice', i));
+        missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, '', missingFiles);
+    end
+    fprintf(fid, '\n');
+%{
+    for i = slices
+        prefix = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'mask_slice', i));
+        missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, '', missingFiles);
+    end
+%}
+    fprintf(fid, '\n');
+    for i = slices
+        prefix = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'bone_slice', i));
+        missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, '', missingFiles);
     end
     fprintf(fid, '\n');
     for i = slices
-        % make filename path and check that the file exists
-        filename = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'mask_slice', i));
-        if exist([filename '.pdf'], 'file') == 0
-            warning('Filename "%s.pdf" does not exist!', filename)
-            missingFiles{length(missingFiles)+1} = [filename '.pdf'];
-        end
-        % Write the line to the tex file
-        fprintf(fid, '  \\subfigure{\\includegraphics[width=0.3\\linewidth]{%s}}\n', filename);
+        prefix = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'cavities_slice', i));
+        missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, '', missingFiles);
     end
-    fprintf(fid, '\n');
-    for i = slices
-        % make filename path and check that the file exists
-        filename = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'bone_slice', i));
-        if exist([filename '.pdf'], 'file') == 0
-            warning('Filename "%s.pdf" does not exist!', filename)
-            missingFiles{length(missingFiles)+1} = [filename '.pdf'];
-        end
-        % Write the line to the tex file
-        fprintf(fid, '  \\subfigure{\\includegraphics[width=0.3\\linewidth]{%s}}\n', filename);
+    fprintf(fid, '  \\caption{%s: Segments. Columns from left to right are slices %d, %d, and %d. Rows are original image, smoothed, bias corrected and cropped image, segmented bone, and segmented cavity.}\n  \\label{fig:%sSegments}\n\\end{figure}\n', fnEsc, slices(1), slices(2), slices(3), fn);
+    
+    fprintf(fid, '\\begin{figure}[H]\n  \\centering\n');
+    prefix = fullfile(pdfPrefix, sprintf('%s_%s', fn, 'implant'));
+    missingFiles = printSubFigure(fid,prefix, '.png', 0.3, '', missingFiles);
+    prefix = fullfile(pdfPrefix, sprintf('%s_%s_rotated', fn, 'zones'));
+    missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, '', missingFiles);
+    fprintf(fid, '  \\\\');
+    for i = 1:2
+    prefix = fullfile(pdfPrefix, sprintf('%s_%sNfraction_%d', fn, 'implant',i));
+    missingFiles = printSubFigure(fid,prefix, '.png', 0.3, '', missingFiles);
     end
-    fprintf(fid, '\n');
-    for i = slices
-        % make filename path and check that the file exists
-        filename = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'cavities_slice', i));
-        if exist([filename '.pdf'], 'file') == 0
-            warning('Filename "%s.pdf" does not exist!', filename)
-            missingFiles{length(missingFiles)+1} = [filename '.pdf'];
-        end
-        % Write the line to the tex file
-        fprintf(fid, '  \\subfigure{\\includegraphics[width=0.3\\linewidth]{%s}}\n', filename);
+    prefix = fullfile(pdfPrefix, sprintf('%s_%sNfraction_all', fn, 'implant'));
+    missingFiles = printSubFigure(fid,prefix, '.png', 0.3, '', missingFiles);
+    fprintf(fid, '  \\caption{%s: Implant and zones.}\n  \\label{fig:%sZones}\n\\end{figure}\n', fnEsc, fn);
+    
+    fprintf(fid, '\\begin{figure}[H]\n  \\centering\n');
+    for i = 1:2
+        prefix = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'bone_fraction', i));
+        missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, sprintf('zone %d',i), missingFiles);
     end
-    fprintf(fid, '  \\caption{%s: Segments. Columns from left to right are slices %d, %d, and %d. Rows are original smoothed image, mask, segmented bone, and segmented cavity.}\n  \\label{fig:%s}\n\\end{figure}\n', fnEsc, slices(1), slices(2), slices(3), fn);
+    prefix = fullfile(pdfPrefix, sprintf('%s_%s_all', fn, 'bone_fraction'));
+    missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, 'All zones', missingFiles);
+    for i = 1:2
+        prefix = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'cavity_fraction', i));
+        missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, sprintf('zone %d',i), missingFiles);
+    end
+    prefix = fullfile(pdfPrefix, sprintf('%s_%s_all', fn, 'cavity_fraction'));
+    missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, 'All zones', missingFiles);
+    fprintf(fid, '  \\caption{%s. Rows are bone and cavity fractions. Columns are zone 1, 2, and all.}\n  \\label{fig:%sFractions}\n\\end{figure}\n', fnEsc, fn);
 
-    fprintf(fid, '\\begin{figure}\n  \\centering\n');
-    % make filename path and check that the file exists
-    filename = fullfile(pdfPrefix, sprintf('%s_%s.pdf', fn, 'edge_effect_bone'));
-    if exist(filename, 'file') == 0
-        warning('Filename "%s" does not exist!', filename)
-        missingFiles{length(missingFiles)+1} = filename;
-    end
-    % Write the line to the tex file
-    fprintf(fid, '  \\subfigure{\\includegraphics[width=0.3\\linewidth]{%s}}\n', filename);
-    fprintf(fid, '  \\caption{%s: Edge effect.}\n  \\label{fig:%s}\n\\end{figure}\n', fnEsc, fn);
-
-    fprintf(fid, '\\begin{figure}\n  \\centering\n');
-    for i = 1:3
-        % make filename path and check that the file exists
-        filename = fullfile(pdfPrefix, sprintf('%s_%s_%d', fn, 'bone_fraction', i));
-        if exist([filename '.pdf'], 'file') == 0
-            warning('Filename "%s.pdf" does not exist!', filename)
-            missingFiles{length(missingFiles)+1} = [filename '.pdf'];
-        end
-        % Write the line to the tex file
-        fprintf(fid, '  \\subfigure[zone %d]{\\includegraphics[width=0.3\\linewidth]{%s}}\n', i, filename);
-    end
-    fprintf(fid, '  \\caption{%s: Bone fractions.}\n  \\label{fig:%s}\n\\end{figure}\n', fnEsc, fn);
-    fprintf(fid, '\\begin{figure}\n  \\centering\n');
-    % make filename path and check that the file exists
-    filename = fullfile(pdfPrefix, sprintf('%s_%s', fn, 'edge_effect_bone'));
-    if exist([filename '.pdf'], 'file') == 0
-        warning('Filename "%s.pdf" does not exist!', filename)
-        missingFiles{length(missingFiles)+1} = [filename '.pdf'];
-    end
-    % Write the line to the tex file
-    fprintf(fid, '  \\subfigure{\\includegraphics[width=0.3\\linewidth]{%s}}\n', filename);
-    fprintf(fid, '  \\caption{%s: Edge effect.}\n  \\label{fig:%s}\n\\end{figure}\n', fnEsc, fn);
-
+    fprintf(fid, '\\begin{figure}[H]\n  \\centering\n');
+    prefix = fullfile(pdfPrefix, sprintf('%s_%s', fn, 'edge_effect_bone'));
+    missingFiles = printSubFigure(fid,prefix, '.pdf', 0.3, '', missingFiles);
+    fprintf(fid, '  \\caption{%s: Edge effect.}\n  \\label{fig:%sEdgeEffect}\n\\end{figure}\n', fnEsc, fn);
+    
     fprintf(fid, '\\clearpage\n');
 end
 if length(missingFiles) > 0
@@ -153,3 +106,16 @@ else
     disp('All referenced imagefiles exists')
 end
 fclose(fid);
+end
+
+function missingFiles = printSubFigure(fid,prefix, suffix, width, caption, missingFiles)
+if exist([prefix suffix], 'file') == 0
+    warning('Filename "%s%s" does not exist!', prefix,suffix)
+    missingFiles{length(missingFiles)+1} = [prefix, suffix];
+else
+    if ~isempty(caption)
+        caption = ['[', caption, ']'];
+    end
+    fprintf(fid, '  \\subfigure%s{\\includegraphics[width=%s\\linewidth]{%s}}\n', caption, num2str(width), prefix);
+end
+end
